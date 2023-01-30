e03b3dfb5f8ee65b4fe2569b5b1ce45c4cfc2fea
Anastasiia Huhtala <anastasiia.huh@gmail.com>
2023-01-30 13:02:48 +0200

    Ready excersise

---
+++ app/src/main/java/com/example/theming/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.theming
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.theming.ui.theme.ThemingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ThemingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val appModifier = Modifier.fillMaxWidth().padding(8.dp)
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "My title",
+            style = MaterialTheme.typography.h5,
+            modifier = appModifier
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = {/*TODO*/},
+            modifier = appModifier
+            )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = appModifier
+            ) {
+            Text(text = "Submit")
+        }
+    }
+    
+}
